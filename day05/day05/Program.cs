b2dde045557d92693e207998cb421a59c9dcca89
Eric Olsson <edolsson@gmail.com>
2023-12-06 00:00:08 -0600

    Completed days 5 and 6

---
+++ day05/day05/Program.cs
@@ -0,0 +1,130 @@
+﻿var puzzleInput = await File.ReadAllLinesAsync("../../../day05-input");
+// var puzzleInput = await File.ReadAllLinesAsync("../../../test-input");
+
+var seeds = ParseSeeds(puzzleInput[0]);
+
+var seedToSoillMap = ParseAlmanacMap("seed-to-soil map", puzzleInput);
+var soilToFertilizerMap = ParseAlmanacMap("soil-to-fertilizer map", puzzleInput);
+var fertilizerToWaterMap = ParseAlmanacMap("fertilizer-to-water map", puzzleInput);
+var waterToLightMap = ParseAlmanacMap("water-to-light map", puzzleInput);
+var lightToTemperatureMap = ParseAlmanacMap("light-to-temperature map", puzzleInput);
+var temperatureToHumidityMap = ParseAlmanacMap("temperature-to-humidity map", puzzleInput);
+var humidityToLocationMap = ParseAlmanacMap("humidity-to-location map", puzzleInput);
+
+var part1 = seeds
+    .Select(s => MapSeed(seedToSoillMap, s))
+    .Select(s => MapSeed(soilToFertilizerMap, s))
+    .Select(s => MapSeed(fertilizerToWaterMap, s))
+    .Select(s => MapSeed(waterToLightMap, s))
+    .Select(s => MapSeed(lightToTemperatureMap, s))
+    .Select(s => MapSeed(temperatureToHumidityMap, s))
+    .Select(s => MapSeed(humidityToLocationMap, s))
+    .Min();
+
+// Console.WriteLine($"Part 1 : {part1}");
+
+static IEnumerable<long> ParseSeeds(string almanacSeeds)
+{
+    var seedSplit = almanacSeeds.Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    var parsedSeeds = seedSplit[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    return parsedSeeds.Select(s => Convert.ToInt64(s));
+}
+
+var allSeedRanges = ParseSeedsForPart2(puzzleInput[0]);
+
+var (_, part2) = Locations(humidityToLocationMap)
+    .Select(loc => ReverseMapValue(humidityToLocationMap, loc, loc))
+    .Select(t => ReverseMapValue(temperatureToHumidityMap, t.Item1, t.Item2))
+    .Select(t => ReverseMapValue(lightToTemperatureMap, t.Item1, t.Item2))
+    .Select(t => ReverseMapValue(waterToLightMap, t.Item1, t.Item2))
+    .Select(t => ReverseMapValue(fertilizerToWaterMap, t.Item1, t.Item2))
+    .Select(t => ReverseMapValue(soilToFertilizerMap, t.Item1, t.Item2))
+    .Select(t => ReverseMapValue(seedToSoillMap, t.Item1, t.Item2))
+    .First(results => SeedExists(allSeedRanges, results.Item1));
+
+Console.WriteLine($"Part 2 {part2}");
+
+static IEnumerable<SeedRange> ParseSeedsForPart2(string almanacSeeds)
+{
+    var seedValues = ParseSeeds(almanacSeeds);
+
+    return seedValues
+        .Chunk(2)
+        .Select(c => CreateSeedRange(c[0], c[1]));
+}
+
+static AlmanacMap ParseAlmanacMap(string almanacName, string[] puzzleInput)
+{
+    var rangeMaps = puzzleInput
+        .SkipWhile(l => !string.Equals(l, $"{almanacName}:", StringComparison.OrdinalIgnoreCase))
+        .Skip(1)
+        .TakeWhile(l => l.Length > 0)
+        .Select(ParseSingleRangeMap)
+        .ToList();
+
+    return new(almanacName, rangeMaps);
+}
+
+
+static RangeMap ParseSingleRangeMap(string rangeMapEntry)
+{
+    var mapElements = rangeMapEntry.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    return new(Convert.ToInt64(mapElements[0]), Convert.ToInt64(mapElements[1]), Convert.ToInt64(mapElements[2]));
+}
+
+static long MapSeed(AlmanacMap map, long seed)
+{
+    var rangeMap = map.Maps.SingleOrDefault(m => m.SourceStart <= seed && seed < m.SourceStart + m.Length);
+
+    //
+    // No map for this seed--just return the seed value
+    //
+    if (rangeMap == default)
+        return seed;
+
+    var offset = seed - rangeMap.SourceStart;
+    return rangeMap.DestinationStart + offset;
+}
+
+static SeedRange CreateSeedRange(long seedStart, long rangeLength)
+    => new(seedStart, rangeLength);
+
+static bool SeedRangeIntersectsMap(SeedRange seeds, RangeMap map)
+    => !(seeds.Start + seeds.Length - 1 < map.SourceStart || map.SourceStart + map.Length < seeds.Start);
+
+static IEnumerable<long> CreateSeedsFromRange(long seedStart, long seedLength)
+{
+    for (var s = seedStart; s < seedStart + seedLength; s++)
+        yield return s;
+}
+
+static (long, long) ReverseMapValue(AlmanacMap map, long input, long location)
+{
+    var rangeMap = map.Maps.SingleOrDefault(m => m.DestinationStart <= input && input < m.DestinationStart + m.Length);
+
+    if (rangeMap == default)
+    {
+        // Console.WriteLine($"    {map.Name} : {input} -> {input}.");
+        return (input, location);
+    }
+
+    var offset = input - rangeMap.DestinationStart;
+    // Console.WriteLine($"    {map.Name} : {input} -> {rangeMap.SourceStart + offset}");
+    return (rangeMap.SourceStart + offset, location);
+}
+
+static IEnumerable<long> Locations(AlmanacMap sourceMap)
+{
+    for (var i = 0L; i < Int64.MaxValue; i++)
+        yield return i;
+}
+
+static bool SeedExists(IEnumerable<SeedRange> seedRanges, long testSeed)
+    => seedRanges.Any(r => r.Start <= testSeed && testSeed < r.Start + r.Length);
+
+record RangeMap(long DestinationStart, long SourceStart, long Length);
+record AlmanacMap(string Name, IReadOnlyCollection<RangeMap> Maps);
+record SeedRange(long Start, long Length);
---
+++ day06/day06/Program.cs
@@ -0,0 +1,100 @@
+﻿// var allRaces = await ParseInputAsync("../../../test-input");
+var allRaces = await ParseInputAsync("../../../day06-input");
+
+/*
+var part1 = allRaces
+    .Select(FindNumberOfWaysToBeatRecord)
+    .Aggregate((a, b) => a * b);
+
+Console.WriteLine($"Part 1 : {part1}");
+*/
+
+// var theOneRace = await ParseInputForPart2Async("../../../test-input");
+var theOneRace = await ParseInputForPart2Async("../../../day06-input");
+var part2 = FindNumberOfWaysToBeatRecordInTheLongRace(theOneRace);
+
+Console.WriteLine($"Part 2 : {part2}");
+
+static long FindNumberOfWaysToBeatRecord(Race race)
+{
+    return Enumerable.Range(1, race.Time)
+        .Select(holdTime => ExceedsBestDistance(race, holdTime))
+        .SkipWhile(result => !result)
+        .TakeWhile(result => result)
+        .LongCount();
+}
+
+static bool ExceedsBestDistance(Race race, int holdTime)
+{
+    //
+    // holdTime = speed (e.g., holdTime of 1 = speed of 1 unit/s; holdTime of 2 = speed of 2 unit/s; etc.
+    //
+    var travelTime = race.Time - holdTime;
+    var distanceTraveled = holdTime * travelTime;
+
+    return distanceTraveled > race.BestDistance;
+}
+
+static async ValueTask<IEnumerable<Race>> ParseInputAsync(string fileName)
+{
+    var allLines = await File.ReadAllLinesAsync(fileName);
+    var timesSplit = allLines[0].Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var allTimes = timesSplit[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+        .Select(t => Convert.ToInt32(t));
+
+    var distancesSplit = allLines[1].Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var allDistances = distancesSplit[1]
+        .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+        .Select(d => Convert.ToInt32(d));
+
+    return allTimes.Zip(allDistances, (time, distance) => new Race(time, distance));
+}
+
+static async ValueTask<LongRace> ParseInputForPart2Async(string fileName)
+{
+    var allLines = await File.ReadAllLinesAsync(fileName);
+    var timesSplit = allLines[0].Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var longTime = Convert.ToInt64(new string(FixBadKerning(timesSplit[1]).ToArray()));
+
+    var distancesSplit = allLines[1].Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var longDistance = Convert.ToInt64(new string(FixBadKerning(distancesSplit[1]).ToArray()));
+
+    return new(longTime, longDistance);
+
+    static IEnumerable<char> FixBadKerning(string input)
+    {
+        foreach (var c in input)
+        {
+            if (Char.IsDigit(c))
+                yield return c;
+        }
+    }
+}
+
+static long FindNumberOfWaysToBeatRecordInTheLongRace(LongRace race)
+{
+    return HoldTimes(race.Time)
+        .Select(t => ExceedsBestLongDistance(race, t))
+        .SkipWhile(r => !r)
+        .TakeWhile(r => r)
+        .LongCount();
+
+    static IEnumerable<long> HoldTimes(long maxTime)
+    {
+        for (var i = 1L; i < maxTime; i++)
+        {
+            yield return i;
+        }
+    }
+}
+
+static bool ExceedsBestLongDistance(LongRace race, long holdTime)
+{
+    var travelTime = race.Time - holdTime;
+    var distance = holdTime * travelTime;
+    return distance > race.BestDistance;
+}
+
+record Race(int Time, int BestDistance);
+
+record LongRace(long Time, long BestDistance);
