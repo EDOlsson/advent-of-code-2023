a9400f89884676e254782c83800f9df91b817c78
Eric Olsson <edolsson@gmail.com>
2023-12-08 23:25:29 -0600

    Complete day 9 part 1

---
+++ day09/Program.cs
@@ -0,0 +1,37 @@
+﻿const string path = "";
+var puzzleInput = await File.ReadAllLinesAsync(Path.Combine(path, "day09-input"));
+// var puzzleInput = await File.ReadAllLinesAsync(Path.Combine(path, "test-input"));
+
+static IEnumerable<int> ParseHistory(string line)
+{
+    return line.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+        .Select(s => Convert.ToInt32(s));
+}
+
+var part1 = puzzleInput
+    .Select(ParseHistory)
+    .Select(PredictReading)
+    .Sum();
+
+Console.WriteLine($"Part 1 : {part1}");
+
+return 0;
+
+static int PredictReading(IEnumerable<int> readings)
+{
+    if (readings.All(r => r is 0))
+        return 0;
+
+    else
+        return readings.Last() + PredictReading(CalculateSingleDifference(readings));
+}
+
+static IEnumerable<int> CalculateSingleDifference(IEnumerable<int> input)
+{
+    var previousReading = input.First();
+    foreach (var reading in input.Skip(1))
+    {
+        yield return reading - previousReading;
+        previousReading = reading;
+    }
+}
